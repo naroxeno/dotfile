03684dacf695db742390e37f289af52d144e57a2
naroxeno <okobenoko@outlook.com>
2024-04-04 16:02:19 +0800

    o

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,37 @@
-function fish_greeting
-  fastfetch -l arch_small
+if status is-interactive
+    alias mkdir='mkdir -p'
+    alias ls='lsd'
+    alias df='duf'
+    alias top='btm'
+    alias du='dust'
+    alias rs='rsync'
+    alias c='clear'
+    alias ci='clear && fish_greeting && cd'
+    alias wine='env LD_PRELOAD="" wine64'
+    # ':' Command
+    alias :q='exit'
+    alias :e='neovide'
+    alias :s='source ~/.config/fish/config.fish'
+    alias :cpkg='sudo pacman -Qtdq | sudo pacman -Rns -'
+    # Dangerous Command 
+    alias rm='rm -i'
+    alias ds='doas'
+    # Git
+    alias gps='git push'
+    alias gpl='git pull'
+    alias gcm='git commit'
+    alias ga='git add'
+    # misc
+    alias mf='musicfox'
+    alias play='mpv --no-audio-display --volume=50'
+    alias watchgpu='watch -n 0.3 nvidia-smi'
+    # Commands to run in interactive sessions can go here
 end
 
-if status is-interactive
-  fish_add_path ~/.local/bin/
-  source ~/.config/fish/utils.fish
-  source ~/.config/fish/alias.fish
+function fish_greeting
+    fastfetch
 end
+
+fish_add_path /opt/usrprogs/bin/ ~/.local/bin/
+
+set -x LANG zh_CN.UTF-8
