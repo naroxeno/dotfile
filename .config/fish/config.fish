6dae53e5d76d36100092597e846785d6ac3eac42
naroxeno <okobenoko@outlook.com>
2025-06-18 11:37:41 +0800

    nothing

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,7 @@ if status is-interactive
     alias rs='rsync'
     alias c='clear'
     alias ci='clear && fish_greeting && cd'
+    alias p='pikaur'
     alias wine='env LD_PRELOAD="" wine64'
     # ':' Command
     alias :q='exit'
@@ -25,6 +26,7 @@ if status is-interactive
     alias mf='musicfox'
     alias play='mpv --no-audio-display --volume=50'
     alias watchgpu='watch -n 0.3 nvidia-smi'
+    alias hx='helix'
     # Commands to run in interactive sessions can go here
 end
 
@@ -32,8 +34,16 @@ function fish_greeting
     fastfetch
 end
 
+function fish_command_not_found --on-event fish_command_not_found
+    eval $(_PR_LAST_COMMAND="$argv" _PR_SHELL="fish" _PR_ALIAS="$(alias)" _PR_MODE="cnf" "pay-respects")
+end
+
+function f -d "Suggest fixes to the previous command"
+    eval $(_PR_LAST_COMMAND="$(history | head -n 1)" _PR_ALIAS="$(alias)" _PR_SHELL="fish" "pay-respects")
+end
+
 fish_add_path /opt/usrprogs/bin/ ~/.local/bin/
 
 set -x LANG zh_CN.UTF-8
-set -x RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
-set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
+set -x RUSTUP_DIST_SERVER https://mirrors.ustc.edu.cn/rust-static
+set -x RUSTUP_UPDATE_ROOT https://mirrors.ustc.edu.cn/rust-static/rustup
